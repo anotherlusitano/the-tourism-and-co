2eb55f61bd76a3e2d8a1df1b86de2644c7194742
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-25 17:41:58 +0100

    Finished Stateful Widgets

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'mocks/mock_location.dart';
 import 'location_list.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final mockLocations = MockLocation.fetchAll();
-    return MaterialApp(home: LocationList(mockLocations));
+    return MaterialApp(home: LocationList());
   }
 }
--- lib/location_list.dart
+++ lib/location_list.dart
@@ -3,47 +3,72 @@ import 'models/location.dart';
 import 'location_detail.dart';
 import 'styles.dart';
 
-class LocationList extends StatelessWidget {
-  final List<Location> locations;
+class LocationList extends StatefulWidget {
+  @override
+  createState() => _LocationListState();
+}
 
-  const LocationList(this.locations, {super.key});
+class _LocationListState extends State<LocationList> {
+  List<Location> locations = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Locations", style: Styles.navBarTitle)),
       body: ListView.builder(
-        itemCount: locations.length,
+        itemCount: this.locations.length,
         itemBuilder: _listViewItemBuilder,
       ),
     );
   }
 
+  loadData() async {
+    final locations = await Location.fetchAll();
+    if (this.mounted) {
+      setState(() {
+        this.locations = locations;
+      });
+    }
+  }
+
   Widget _listViewItemBuilder(BuildContext context, int index) {
-    var location = locations[index];
+    final location = this.locations[index];
     return ListTile(
-        contentPadding: const EdgeInsets.all(10.0),
-        leading: _itemThumbnail(location),
-        title: _itemTitle(location),
-        onTap: () => _navigateToLocationDetail(context, index));
-  }
-
-  void _navigateToLocationDetail(BuildContext context, int locationID) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => LocationDetail(locationID),
-        ));
-  }
-
-  Widget _itemThumbnail(Location location) {
-    return Container(
-      constraints: const BoxConstraints.tightFor(width: 100.0),
-      child: Image.network(location.url, fit: BoxFit.fitWidth),
+      contentPadding: EdgeInsets.all(10.0),
+      leading: _itemThumbnail(this.locations[index]),
+      title: _itemTitle(this.locations[index]),
+      onTap: () => _navigateToLocationDetail(context, location.id),
     );
   }
 
+  void _navigateToLocationDetail(BuildContext context, int locationID) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => LocationDetail(locationID)));
+  }
+
+  Widget _itemThumbnail(Location location) {
+    if (location.url.isEmpty) {
+      return Container();
+    }
+
+    try {
+      return Container(
+        constraints: BoxConstraints.tightFor(height: 100.0),
+        child: Image.network(location.url, fit: BoxFit.fitWidth),
+      );
+    } catch (e) {
+      print("could not load image ${location.url}");
+      return Container();
+    }
+  }
+
   Widget _itemTitle(Location location) {
-    return Text(location.name, style: Styles.textDefault);
+    return Text('${location.name}', style: Styles.textDefault);
   }
 }
